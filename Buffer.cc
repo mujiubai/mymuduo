987c030d7f0c6dc467c65aa6c65faa5a318765e8
1057378931@qq.com <enwu@DESKTOP-A0MI3IN.localdomain>
2022-11-04 21:46:51 +0800

    实现Buffer类

---
+++ Buffer.cc
@@ -0,0 +1,47 @@
+#include "Buffer.h"
+
+#include <errno.h>
+#include <sys/uio.h>
+
+using namespace muduo;
+
+/**
+ * @brief 从fd上读取数据  低层的Poller工作在LT模式
+ * buffer缓冲区有大小，但是从fd上读数据时，却不知道tcp数据最终的大小，
+ * 如果不断将buffer进行扩容，效率很低;而如果预先就设为很大空间，当用户数量很多时，内存容量消耗极大
+ * 因此，使用一个栈上临时变量来存储现有缓冲区不够存的数据，最后将其写入缓冲区中
+ * @param fd sockfd
+ * @param saveErrno 错误
+ * @return size_t 成功读取大小
+ */
+size_t Buffer::readFd(int fd, int* saveErrno) {
+  char extrabuf[65536] = {0};  //栈上空间 64k
+  struct iovec vec[2];
+  // buffer缓冲区剩余的可写空间大小
+  const size_t writeable = writeableBytes();
+  vec[0].iov_base = beginWrite();
+  vec[0].iov_len = writeable;
+
+  vec[1].iov_base = extrabuf;
+  vec[1].iov_len = sizeof extrabuf;
+
+//当缓冲区可写空间小于extrabuf时，使用buffer和extrabuf一起作为readv写入的空间
+//反之，如果可写空间大于extrabuf空间时，就没有必要再使用一个extrabuf来作为暂存了
+//因为buffer在写入数据时，如果空间不够会自动扩容为原来2倍，而即使加上一个extrabuf能存储的数据也没有扩容后的大
+//这样效率效率还没有直接使用一个buffer让其自动扩容效率高，毕竟buffer resize时也会进行依次复制
+//extrabuf的使用原因是为了避免buffer不断扩容而导致效率低
+//这里不用担心如果空间不够存，使用的是LT模式，没读完的数据后面会不断唤醒读
+  const int iovcnt = (writeable < sizeof extrabuf) ? 2 : 1;
+  //readv能自动将数据写入到多个缓冲区中，缓冲区都在vec数组中
+  const size_t n = ::readv(fd, vec, iovcnt);
+  if (n < 0) {
+    *saveErrno = errno;
+  } else if (n <= writeable) {//buffer的缓冲区够存储数据
+    writerIndex_ += n;
+  } else {  // extrabuf里也写入了数据
+    writerIndex_ = buffer_.size();
+    //将extrabuf追加到buffer中
+    append(extrabuf, n - writeable);
+  }
+  return n;
+}
--- Buffer.h
+++ Buffer.h
@@ -1,6 +1,107 @@
 #pragma once
 
+#include <stddef.h>
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
 namespace muduo {
 
-class Buffer {};
+/// A buffer class modeled after org.jboss.netty.buffer.ChannelBuffer
+///
+/// @code
+/// +-------------------+------------------+------------------+
+/// | prependable bytes |  readable bytes  |  writable bytes  |
+/// |                   |     (CONTENT)    |                  |
+/// +-------------------+------------------+------------------+
+/// |                   |                  |                  |
+/// 0      <=      readerIndex   <=   writerIndex    <=     size
+//缓冲区类
+class Buffer {
+ public:
+  static const size_t kCheapPrepend = 8;
+  static const size_t kInitialSize = 1024;
+  explicit Buffer(size_t initialSize = kInitialSize)
+      : buffer_(kCheapPrepend + initialSize),
+        readerIndex_(kCheapPrepend),
+        writerIndex_(kCheapPrepend) {}
+
+  //可读数据长度
+  size_t readableBytes() const { return writerIndex_ - readerIndex_; }
+  //可写数据长度
+  size_t writeableBytes() const { return buffer_.size() - writerIndex_; }
+  //预留空间长度，初始的预留空间是为消息长度而准备
+  size_t prependableBytes() const { return readerIndex_; }
+
+  //返回缓冲区中可读地址的起始地址
+  const char* peek() const { return begin() + readerIndex_; }
+
+  //读取len长度后调用此函数
+  void retrieve(size_t len) {
+    //如果数据没读完
+    if (len < readableBytes()) {
+      readerIndex_ += len;  //应用只读取了可读缓冲区的一部分，
+    } else {
+      retrieveAll();  //已经读完，需将缓冲区复位为初始状态
+    }
+  }
+  void retrieveAll() { readerIndex_ = writerIndex_ = kCheapPrepend; }
+
+  //把onMessage函数上报的buffer数据，转成string类型返回
+  std::string retrieveAllAsString() {
+    return retrieveAsString(readableBytes());
+  }
+
+  std::string retrieveAsString(size_t len) {
+    std::string result(peek(), len);
+    //上面已经把缓冲区的数据读出，需要对缓冲区进行复位操作
+    retrieve(len);
+    return result;
+  }
+
+  //确保len长度的数据能被写入
+  void ensureWriteableBytes(size_t len) {
+    if (writeableBytes() < len) {
+      makeSpace(len);
+    }
+  }
+
+  //从fd上读取数据
+  size_t readFd(int fd, int* saveErrno);
+
+ private:
+  //返回buffer首元素地址，即数组的起始地址，&*不能抵消，因为*被重写过
+  char* begin() { return &*buffer_.begin(); }
+
+  const char* begin() const { return &*buffer_.begin(); }
+
+  //通过整理buffer空间或增加buffer空间使得容量够写
+  void makeSpace(size_t len) {
+    //如果可写空间加上前面预留空间（预留空间可能由于依次没读完而边长）还是小于要求的长度
+    if (writeableBytes() + prependableBytes() < len + kCheapPrepend) {
+      buffer_.resize(len + len);
+    } else {
+      //如果数据空间够用，则将现有数据前移
+      size_t readable = readableBytes();
+      std::copy(begin() + readerIndex_, begin() + writeableBytes(),
+                begin() + kCheapPrepend);
+    }
+  }
+
+  //把data中的len长的数据添加到缓冲区中
+  void append(const char* data, size_t len) {
+    ensureWriteableBytes(len);
+    std::copy(data, data + len, beginWrite());
+    writerIndex_ += len;
+  }
+
+  //返回可写处的指针
+  char* beginWrite() { return begin() + writerIndex_; }
+  const char* beginWrite() const { return begin() + writerIndex_; }
+
+  std::vector<char> buffer_;
+  size_t readerIndex_;
+  size_t writerIndex_;
+};
 }  // namespace muduo
